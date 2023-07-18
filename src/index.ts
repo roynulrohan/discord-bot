8860c57d78f7a50ad183905f75261f42350cefd8
Roynul Rohan <roynulrohan@gmail.com>
2023-07-17 21:43:32 -0400

    yes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-import { Client, GuildMember, GatewayIntentBits, ActivityType } from 'discord.js';
-import { Player, QueryType, Queue, Track } from 'discord-player';
-import axios from 'axios';
+import { Client, GuildMember, GatewayIntentBits } from 'discord.js';
+import { EqualizerConfigurationPreset, Player, QueryType } from 'discord-player';
 import { Commands } from './commands';
 
 require('dotenv').config();
@@ -16,48 +15,15 @@ const client = new Client({
 });
 
 const prefix = '-';
-const bakrApiUrl = 'https://bakraas.com/';
 const commands = new Commands();
-
-const denyMessages = ['Fuck off', 'Fuck you', 'No', "Nah don't think so", 'Lol fuck off'];
 
 client.on('ready', async () => {
     console.log(`${client.user?.tag} is online!`);
 
-    // const guilds = await client.guilds.fetch();
-    // guilds.forEach(async (guild) => {
-    //     const fetchedGuild = await guild.fetch();
-    //     fetchedGuild.commands
-    //         .set([
-    //             {
-    //                 name: 'play',
-    //                 description: 'Plays a song from youtube',
-    //                 options: [
-    //                     {
-    //                         name: 'query',
-    //                         type: 3,
-    //                         description: 'The song you want to play',
-    //                         required: true,
-    //                     },
-    //                 ],
-    //             },
-    //             {
-    //                 name: 'skip',
-    //                 description: 'Skip to the current song',
-    //             },
-    //             {
-    //                 name: 'stop',
-    //                 description: 'Stop the player',
-    //             },
-    //         ])
-    //         .then((res) => {
-    //             console.log(`messages deployed for ${fetchedGuild.name}`);
-    //         });
-    // });
-
     client.user?.setStatus('dnd');
-    client.user?.setPresence({ activities: [{ name: 'with MEE6' }] });
+    client.user?.setPresence({ activities: [{ name: "with YGCamel's balls" }] });
 });
+
 client.on('error', console.error);
 client.on('warn', console.warn);
 
@@ -69,32 +35,45 @@ const player = new Player(client, {
     },
 });
 
-player.on('error', (queue, error) => {
-    console.log(`[${queue.guild.name}] Error emitted from the queue: ${error.message}`);
+player.events.on('error', (queue, error) => {
+    // Emitted when the player queue encounters error
+    console.log(`General player error event: ${error.message}`);
+    console.log(error);
 });
 
-player.on('connectionError', (queue, error) => {
-    console.log(`[${queue.guild.name}] Error emitted from the connection: ${error.message}`);
+player.events.on('playerError', (queue, error) => {
+    // Emitted when the audio player errors while streaming audio track
+    console.log(`Player error event: ${error.message}`);
+    console.log(error);
 });
 
-player.on('trackStart', (queue: Queue<any>, track) => {
-    queue.metadata?.send(`🎶 | Started playing: **${track.title} - ⏱ ${track.duration}** in **${queue.connection.channel.name}**!`);
+player.events.on('playerStart', (queue: any, track) => {
+    queue.filters.equalizer?.setEQ(EqualizerConfigurationPreset.Flat);
+    queue.metadata.channel.send(`🎶 | Started playing: **${track.title} - ⏱ ${track.duration}** in **${queue.channel.name}**!`);
 });
 
-player.on('trackAdd', (queue: Queue<any>, track) => {
-    queue.metadata.send(`🎶 | Track **${track.title} - ⏱ ${track.duration}** queued!`);
+player.events.on('playerSkip', (queue: any, track) => {
+    queue.metadata.channel.send(`🎶 | **${track.title} - ⏱ ${track.duration}** was skipped.`);
 });
 
-player.on('botDisconnect', (queue: Queue<any>) => {
-    queue.metadata.send('❌ | I was manually disconnected from the voice channel, clearing queue!');
+player.events.on('audioTrackAdd', (queue: any, track) => {
+    queue.metadata.channel.send(`🎶 | Track **${track.title} - ⏱ ${track.duration}** queued!`);
 });
 
-player.on('channelEmpty', (queue: Queue<any>) => {
-    queue.metadata.send('❌ | Nobody is in the voice channel, leaving...');
+player.events.on('audioTracksAdd', (queue: any, tracks) => {
+    queue.metadata.channel.send(`🎶 | ${tracks.length} tracks queued!`);
 });
 
-player.on('queueEnd', (queue: Queue<any>) => {
-    queue.metadata.send('✅ | Queue finished!');
+player.events.on('disconnect', (queue: any) => {
+    queue.metadata.channel.send('❌ | I was disconnected from the voice channel, clearing queue!');
+});
+
+player.events.on('emptyChannel', (queue: any) => {
+    queue.metadata.channel.send('❌ | Nobody is in the voice channel, leaving...');
+});
+
+player.events.on('emptyQueue', (queue: any) => {
+    queue.metadata.channel.send('✅ | Queue finished!');
 });
 
 client.on('messageCreate', async (message) => {
@@ -103,13 +82,6 @@ client.on('messageCreate', async (message) => {
     if (!message.content.startsWith(prefix)) return;
 
     const command = message.content.replace(prefix, '').split(' ')[0].trim();
-
-    const rand = Math.floor(Math.random() * 10);
-
-    if (message.member?.id === '476064362537156619' && rand === 5) {
-        message.reply({ content: denyMessages[Math.floor(Math.random() * denyMessages.length)] });
-        return;
-    }
 
     if (commands.isMusicCommand(command)) {
         if (!(message.member instanceof GuildMember) || !message.member.voice.channel) {
@@ -136,43 +108,47 @@ client.on('messageCreate', async (message) => {
         if (!message.guild) return void console.log('unknown error');
 
         const queue =
-            player.getQueue(message.guildId) ||
-            player.createQueue(message.guild, {
-                metadata: message.channel,
-                autoSelfDeaf: false,
-                spotifyBridge: true,
+            player.queues.get(message.guildId) ||
+            player.queues.create(message.guildId, {
+                metadata: {
+                    channel: message.channel,
+                    client: message.guild.members.me,
+                    requestedBy: message.author,
+                },
                 leaveOnEnd: false,
+                selfDeaf: false,
+                skipOnNoStream: true,
             });
 
         try {
             if (!message.member?.voice.channelId) return;
             if (!queue.connection) await queue.connect(message.member.voice.channelId);
         } catch {
-            void player.deleteQueue(message.guildId);
+            void player.queues.delete(message.guildId);
             message.channel.send({ content: 'Could not join your voice channel!' });
             return;
         }
 
         await message.channel.send({ content: `⏱ | Loading your ${searchResult.playlist ? 'playlist' : 'track'}...` });
-        searchResult.playlist ? queue.addTracks(searchResult.tracks) : queue.addTrack(searchResult.tracks[0]);
-        if (!queue.playing) {
-            await queue.play();
+        queue.addTrack(searchResult.playlist ? searchResult.tracks : searchResult.tracks[0]);
+        if (!queue.isPlaying()) {
+            queue.node.play();
         }
     } else if (commands.isSkip(command)) {
-        const queue = player.getQueue(message.guildId);
-        if (!queue || !queue.playing) return void message.channel.send({ content: '❌ | No music is being played!' });
-        const currentTrack = queue.current;
-        const success = queue.skip();
+        const queue = player.queues.get(message.guildId);
+        if (!queue || !queue.isEmpty) return void message.channel.send({ content: '❌ | No music is being played!' });
+        const currentTrack = queue.currentTrack;
+        const success = queue.node.skip();
         return void message.channel.send({
             content: success ? `✅ | Skipped **${currentTrack}**!` : '❌ | Something went wrong!',
         });
     } else if (commands.isStop(command)) {
-        const queue = player.getQueue(message.guildId);
-        if (!queue || !queue.playing) return void message.channel.send({ content: '❌ | No music is being played!' });
-        queue.destroy();
+        const queue = player.queues.get(message.guildId);
+        if (!queue || !queue.isPlaying()) return void message.channel.send({ content: '❌ | No music is being played!' });
+        queue.delete();
         return void message.channel.send({ content: '🛑 | Stopped the player!' });
     } else if (commands.isQueue(command)) {
-        const queue = player.getQueue(message.guildId);
+        const queue = player.queues.get(message.guildId);
         if (!queue) return;
         const list = queue.tracks.map((track, i) => {
             return `**${i + 1}: ${track}**`;
@@ -181,15 +157,15 @@ client.on('messageCreate', async (message) => {
         if (list.length === 0) return void message.channel.send({ content: '❌ | Queue is empty' });
         return void message.channel.send({ content: `**Current Queue**\n\n${list.join('\n')}` });
     } else if (commands.isClear(command)) {
-        const queue = player.getQueue(message.guildId);
+        const queue = player.queues.get(message.guildId);
         if (!queue) return;
         queue.clear();
         return void message.channel.send({ content: '✅ | Cleared the queue!' });
     } else if (commands.isSeek(command)) {
-        const queue = player.getQueue(message.guildId);
-        if (!queue || !queue.playing) return;
+        const queue = player.queues.get(message.guildId);
+        if (!queue || !queue.isPlaying()) return;
         const query: any = message.content.replace(prefix, '').split(' ')[1].trim();
-        const success = await queue.seek(Math.ceil(parseInt(query) * 1000));
+        const success = await queue.node.seek(Math.ceil(parseInt(query) * 1000));
         return void message.channel.send({
             content: `${success ? `✅ | Seeked to ${query}` : `❌ | Something went wrong!`}`,
         });
